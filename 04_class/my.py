6585dfb9a48290ff5e738833720cc06e979782ae
megumeguro <krokomeguro@gmail.com>
2021-09-24 18:32:27 +0900

    modify my answer, write check code

--- 04_class/my.py
+++ 04_class/my.py
@@ -5,23 +5,25 @@ import unittest
 class Bot():
 
     def __init__(self, owner_name):
-        """
-        WRITE YOUR CODE HERE
-        """
+        self.owner_name = owner_name
 
     def reply(self, call):
-        """
-        out = self.owner_name
-        """
+        if self.owner_name in call:
+            return 'Hello my Boss!'
+        else:
+            return 'Hello neighbor'
 
 
 class TestBot(unittest.TestCase):
 
     def test_bot_reply(self):
         bot = Bot("Angy")
-        self.assertEqual("Hello", bot.reply("Hi, I'm Bill.'"))
+        self.assertEqual("Hello neighbor", bot.reply("Hi, I'm Bill.'"))
         self.assertEqual("Hello my Boss!", bot.reply("Hi, I'm Angy."))
 
 
 if __name__ == "__main__":
     unittest.main()
+
+
+print(Bot('Joe').reply('Hello Joe san!'))
